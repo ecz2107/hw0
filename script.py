9aebc193523390be7e3858b9fb003aa1441c37e6
ecz2107 <ecz2107@columbia.edu>
2015-09-14 15:07:20 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
-import csv 
-with open ('iowa-liquor...', 'r') as csvfile:
-	count = 0
-	for row in csvfile:
-		if 'single malt scotch' in row.lower
-			count +=1
+import csv
+with open ('iowa-liquor-sample.csv', 'r') as csvfile:
+        count = 0
+        for row in csvfile:
+                if 'single malt scotch' in row.lower():
+                        count +=1
 print count
-		
